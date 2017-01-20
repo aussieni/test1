c503a083904f60562ab626bab95048d1a7e4beef
Hew Wolff <hewwolff@sonic.net>
2017-01-20 10:52:36 -0800

    add arc length

--- quote.rb
+++ quote.rb
@@ -11,7 +11,7 @@ class Quote
       end
       center = e['Center'] && [e['Center']['X'], e['Center']['Y']]
       clockwise_from_index = e['ClockwiseFrom'] && e['Vertices'].index(e['ClockwiseFrom'])
-      Edge.new(vertices, center, clockwise_from_index)
+      Edge.create(vertices, center, clockwise_from_index)
     end
   end
 
@@ -19,22 +19,73 @@ class Quote
 end
 
 class Edge
-  def initialize(vertices, center = nil, clockwise_from_index = nil)
+  def self.create(vertices, center = nil, clockwise_from_index = nil)
+    center ?
+      Arc.new(vertices, center, clockwise_from_index) :
+      LineSegment.new(vertices)
+  end
+
+  attr_reader :vertices
+
+  def time_cost(cost_params)
+    puts "length: #{length}"
+    puts "speed: #{speed(cost_params.max_speed)}"
+    (length / speed(cost_params.max_speed)) * cost_params.time_cost
+  end
+
+  def distance(p0, p1)
+    Math.sqrt((p1[0] - p0[0]) ** 2 + (p1[1] - p0[1]) ** 2)
+  end
+
+  private
+  
+  def initialize(vertices)
     @vertices = vertices
+  end
+end
+
+class LineSegment < Edge
+  def is_arc
+    false
+  end
+
+  def length
+    distance(vertices[0], vertices[1])
+  end
+
+  def speed(max_speed)
+    max_speed
+  end
+end
+
+class Arc < Edge
+  def initialize(vertices, center, clockwise_from_index)
+    super(vertices)
     @center = center
     @clockwise_from_index = clockwise_from_index
   end
 
+  attr_reader :center, :clockwise_from_index
+
   def is_arc
-    !!center
+    true
   end
 
-  attr_reader :vertices, :center, :clockwise_from_index
+  def length
+    shifted_vertices = vertices.map { |v| [v[0] - center[0], v[1] - center[1]] }
+    angles = shifted_vertices.map { |v| Math::atan2(v[1], v[0]) }
+    cc_angle_diff = angles[1] - angles[0]
+    angle_diff = clockwise_from_index == 0 ? -cc_angle_diff : cc_angle_diff
+    angle_distance = angle_diff.modulo(2 * Math::PI)
+    angle_distance * radius
+  end
 
-  def time_cost(cost_params)
-    length = Math.sqrt((vertices[1][0] - vertices[0][0]) ** 2 +
-                       (vertices[1][1] - vertices[0][1]) ** 2)
-    (length / cost_params.max_speed) * cost_params.time_cost
+  def speed(max_speed)
+    max_speed * Math.exp(-1 / radius)
+  end
+
+  def radius
+    distance(vertices[0], center)
   end
 end
 
--- test_quote.rb
+++ test_quote.rb
@@ -3,14 +3,14 @@ require 'test/unit'
  
 class TestQuote < Test::Unit::TestCase
   def setup
-    @cost_params = CostParams.new(0.1, 0.75, 0.5, 0.07)
+    @cost_params = CostParams.new(0.1, 0.75, 0.5, 0.01)
   end
   
   def test_parse
     json = IO.read('CutCircularArc.json')
     quote = Quote.new(json)
-    
     edges = quote.edges
+    
     assert_equal(4, edges.length)
 
     edge0 = edges[0]
@@ -25,7 +25,20 @@ class TestQuote < Test::Unit::TestCase
   end
 
   def test_machine_cost_line_segment
-    e = Edge.new([[0, 1], [2, 0]])
-    assert_equal((Math.sqrt(5) / 0.5) * 0.07, e.time_cost(@cost_params))
+    e = Edge.create([[0, 1], [2, 0]])
+    assert_equal((Math.sqrt(5) / 0.5) * 0.01, e.time_cost(@cost_params))
+  end
+
+  def test_machine_cost_arc
+    speed = 0.5 * Math.exp(-2)
+    full_circle = 2 * Math::PI * 0.5
+
+    edge0 = Edge.create([[0, -1], [-0.5, -0.5]], [0, -0.5], 1)
+    length = full_circle * (3.0/4.0)
+    assert_equal((length / speed) * 0.01, edge0.time_cost(@cost_params))
+
+    edge1 = Edge.create([[0, -1], [-0.5, -0.5]], [0, -0.5], 0)
+    length = full_circle * (1.0/4.0)
+    assert_equal((length / speed) * 0.01, edge1.time_cost(@cost_params))
   end
 end
